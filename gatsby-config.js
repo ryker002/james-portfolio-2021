2248272e4972a68f279037e8bffb0ac51fd48521
James <jamesrlew@gmail.com>
2022-02-16 14:12:03 -0600

    Remove Firebase Temporarily for Temp Deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,20 +93,20 @@ module.exports = {
         contentApiKey: process.env.GHOST_CONTENT_API_KEY,
       },
     },
-    {
-      resolve: "gatsby-plugin-firebase",
-      options: {
-        credentials: {
-          apiKey: process.env.FIREBASE_APIKEY,
-          authDomain: process.env.FIREBASE_AUTHDOMAIN,
-          databaseURL: process.env.FIREBASE_DATABASEURL,
-          projectId: process.env.FIREBASE_PROJECTID,
-          storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-          messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
-          appId: process.env.FIREBASE_APPID,
-          measurementId: process.env.FIREBASE_MEASUREMENTID,
-        },
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-firebase",
+    //   options: {
+    //     credentials: {
+    //       apiKey: process.env.FIREBASE_APIKEY,
+    //       authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    //       databaseURL: process.env.FIREBASE_DATABASEURL,
+    //       projectId: process.env.FIREBASE_PROJECTID,
+    //       storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    //       messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+    //       appId: process.env.FIREBASE_APPID,
+    //       measurementId: process.env.FIREBASE_MEASUREMENTID,
+    //     },
+    //   },
+    // },
   ],
 }
--- src/components/user/userButton.js
+++ src/components/user/userButton.js
@@ -17,65 +17,67 @@ import {
   Button,
 } from "@chakra-ui/react"
 import { FaUserAlt, FaLock } from "react-icons/fa"
-import firebase from "gatsby-plugin-firebase"
-import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth"
-import { useAuthState } from "react-firebase-hooks/auth"
+// import firebase from "gatsby-plugin-firebase"
+// import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth"
+// import { useAuthState } from "react-firebase-hooks/auth"
 
 export function LoginButton() {
-  const auth = getAuth(firebase)
+  // const auth = getAuth(firebase)
   // const [userName, setUserName] = useState();
   // const [password, setPassword] = useState();
-  const username = useRef()
-  const password = useRef()
-  const login = () => {
-    console.log(username.current.target.value)
-    console.log(password.current.target.value)
-    // signInWithEmailAndPassword(auth, "test@test.com", "password")
-  }
+  // const username = useRef()
+  // const password = useRef()
+  // const login = () => {
+  //   console.log(username.current.target.value)
+  //   console.log(password.current.target.value)
+  //   // signInWithEmailAndPassword(auth, "test@test.com", "password")
+  // }
 
-  const logout = () => {
-    signOut(auth)
-  }
-  const [user, loading, error] = useAuthState(auth)
-  return (
-    <Popover>
-      <PopoverTrigger>
-        <IconButton
-          colorScheme="transparent"
-          onClick={null}
-          color="white"
-          icon={<FaUserAlt size="22" />}
-        />
-      </PopoverTrigger>
-      <PopoverContent>
-        <PopoverArrow />
-        <PopoverCloseButton />
-        <PopoverHeader>Login</PopoverHeader>
-        <PopoverBody as="form">
-          <Stack spacing={4}>
-            <InputGroup>
-              <InputLeftElement
-                pointerEvents="none"
-                children={<FaUserAlt color="gray.300" />}
-              />
-              <Input
-                type="email"
-                placeholder="email@address.com"
-                ref={username}
-              />
-            </InputGroup>
+  // const logout = () => {
+  //   signOut(auth)
+  // }
+  // const [user, loading, error] = useAuthState(auth)
+  // return (
+  //   <Popover>
+  //     <PopoverTrigger>
+  //       <IconButton
+  //         colorScheme="transparent"
+  //         onClick={null}
+  //         color="white"
+  //         icon={<FaUserAlt size="22" />}
+  //       />
+  //     </PopoverTrigger>
+  //     <PopoverContent>
+  //       <PopoverArrow />
+  //       <PopoverCloseButton />
+  //       <PopoverHeader>Login</PopoverHeader>
+  //       <PopoverBody as="form">
+  //         <Stack spacing={4}>
+  //           <InputGroup>
+  //             <InputLeftElement
+  //               pointerEvents="none"
+  //               children={<FaUserAlt color="gray.300" />}
+  //             />
+  //             <Input
+  //               type="email"
+  //               placeholder="email@address.com"
+  //               ref={username}
+  //             />
+  //           </InputGroup>
 
-            <InputGroup>
-              <InputLeftElement
-                pointerEvents="none"
-                children={<FaLock color="gray.300" />}
-              />
-              <Input type="password" ref={password} />
-            </InputGroup>
-            <Button onClick={login}>Login</Button>
-          </Stack>
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
-  )
+  //           <InputGroup>
+  //             <InputLeftElement
+  //               pointerEvents="none"
+  //               children={<FaLock color="gray.300" />}
+  //             />
+  //             <Input type="password" ref={password} />
+  //           </InputGroup>
+  //           <Button onClick={login}>Login</Button>
+  //         </Stack>
+  //       </PopoverBody>
+  //     </PopoverContent>
+  //   </Popover>
+  // )
+
+  return null
 }
